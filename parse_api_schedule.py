4dd04f929197be16f6b7c956968d92f6d7e2e36a
Coding <Coding@Jonathans-MacBook-Pro.local>
2017-12-13 15:22:31 -0800

    Typo

--- parse_api_schedule.py
+++ parse_api_schedule.py
@@ -54,7 +54,7 @@ json = r.json()
 
 pprint(json)
 
-sql = """INSERT INTO schedule(game_id, season, home_id, away_id, venue, date, time)
+sql = """INSERT INTO schedule(game_id, season, home_team, away_team, venue, date, time)
          VALUES(%s, %s, %s, %s, %s, %s, %s);"""
 
 for games_date in json['dates']:
@@ -87,7 +87,7 @@ for games_date in json['dates']:
 			venue = game['venue']['name']
 
 			# execute the INSERT statement
-			cur.execute(sql, (game_id, season, home_team, away_team, venue, date, time))
+			cur.execute(sql, (game_id, season, home_id, away_id, venue, date, time))
 
 # close communication with the PostgreSQL database server
 cur.close()
